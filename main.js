af33459fbb08db7fb263fa710fd70f8369e58307
Last Breath Phase 3 <106328018+Papyrusdopastel@users.noreply.github.com>
2024-03-18 07:59:26 -0300

    aa3

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; i++) {
 
     botoes[i].onclick = function () {
-
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
